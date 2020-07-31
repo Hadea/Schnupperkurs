4983e2ff4e71b96235860fd30a2d39f23e879b47
Lutz <lambda2@gmx.at>
2020-07-31 13:57:15 +0200

    Bugfix for collision

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -9,15 +9,15 @@ public class PlayerControl : MonoBehaviour
     [SerializeField]
     private float PlayerRotationSpeed;
 
-    void Update()
+    void FixedUpdate()
     {
-        transform.Translate(Vector3.forward * PlayerForwardSpeed* Time.deltaTime * Input.GetAxis("Vertical"));
-        transform.Rotate(Vector3.up * PlayerRotationSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+        transform.Translate(Vector3.forward * PlayerForwardSpeed* Time.fixedDeltaTime * Input.GetAxis("Vertical"));
+        transform.Rotate(Vector3.up * PlayerRotationSpeed * Time.fixedDeltaTime * Input.GetAxis("Horizontal"));
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger wurde ausgelöst, raumsich wird zerstört");
+        Debug.Log("Trigger wurde ausgelöst, raumschiff wird zerstört");
         Camera.main.transform.parent = null;
         Destroy(this.gameObject);
     }
