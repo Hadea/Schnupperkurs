7aea5cfc4d05204fe728af3e181c15f023461466
Lutz <lambda2@gmx.at>
2020-07-31 10:13:13 +0200

    Unity project setup

---
+++ Assets/Scripts/RotationPermanent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationPermanent : MonoBehaviour
+{
+    [SerializeField] 
+    private Vector3 RotationSpeed = new Vector3(30, 30, 30);
+    void Update()
+    {
+        transform.Rotate(RotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/TranslationPermanent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class TranslationPermanent : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 MoveSpeed = Vector3.zero;
+    void Update()
+    {
+        transform.Translate(MoveSpeed * Time.deltaTime, Space.World);
+    }
+}
