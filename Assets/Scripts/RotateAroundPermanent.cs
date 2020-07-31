0ed5c38c01f78a46e1b0cba2c4af97008e89cbb3
Lutz <lambda2@gmx.at>
2020-07-31 11:26:04 +0200

    Added orbit to planets

---
+++ Assets/Scripts/RotateAroundPermanent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundPermanent : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ZielObjekt;
+    [SerializeField]
+    private float RotationsTempo;
+    void Update()
+    {
+        transform.RotateAround(ZielObjekt.transform.position, Vector3.up, RotationsTempo * Time.deltaTime);
+    }
+}
